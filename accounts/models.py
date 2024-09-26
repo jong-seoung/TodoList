e582aa967be8df73c57582c6c43b879a8639ea16
beak jong seoung <jjong015189@gmail.com>
2024-09-26 18:10:15 +0900

    Profile-nickname 추가 & 회원가입 후,자동 로그인 안하도록 설정

--- accounts/models.py
+++ accounts/models.py
@@ -8,4 +8,5 @@ class User(AbstractUser):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=50, blank=True)
     avatar = models.ImageField(blank=True)
--- accounts/views.py
+++ accounts/views.py
@@ -12,9 +12,8 @@ class SignupView(APIView):
     def post(self, request):
         serializer = SignupSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
-            auth_login(request, user)
-            return Response({"message": "회원가입 성공 및 자동 로그인 완료"}, status=status.HTTP_201_CREATED)
+            serializer.save()
+            return Response({"message": "회원가입 성공"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
