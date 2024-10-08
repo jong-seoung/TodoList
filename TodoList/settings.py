2617c9ad32674d7ba13175628ea543f7ba1a0661
root <root@ip-10-0-1-152.ap-northeast-2.compute.internal>
2024-10-08 04:09:36 +0000

    allow host, 데이터 베이스 설정

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY", default="django-insecure-stfx7qb!3msu9)!qmdagr370
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
